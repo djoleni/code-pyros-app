33b44af1786c3695cd2dc63d911fd6c751e40852
Djoleni <stevanojlo@gmail.com>
2025-02-11 00:43:04 +0100

    webhooks for auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import { SignOutButton, SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      
+      <SignUpButton/>
 
 
-
+      <SignOutButton/>
 
     </div>
   );
